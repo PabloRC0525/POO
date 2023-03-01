8bbd1ff9ab92988ad17fb46a02ac30ffbfae7e0b
PabloRC0525 <121038827@upq.edu.mx>
2023-03-01 14:59:53 -0600

    Funcionalidades Botones, messagebox, show , ask

--- Tkinther/Practica11.py
+++ Tkinther/Practica11.py
@@ -1,5 +1,17 @@
 from tkinter import *
-
+from tkinter import messagebox
+#5 agregar funcion de mensaje
+def mostrarmensaje():
+    messagebox.showinfo("Informacion","Te informo que aqui todo esta peor que al igual que ella mi voluntad tambien murio")
+    messagebox.showerror("Error","Te he fallado Anakin, te he fallado!!!")
+    print(messagebox.askokcancel("Pregunta","¿Seguro que quieres guardar?"))
+    
+#6 funcion agrear botones
+def agregarboton():
+        botonVerde.config(text="+",bg="lightgreen",fg="black")
+        botonNuevo=Button(seccion3, text="Boton nuevo",bg="black",fg="white")
+        botonNuevo.pack()
+    
 #Generar ventana
 ventana = Tk()
 ventana.title("Ejemplo 3 Frames")
@@ -16,7 +28,7 @@ seccion3=Frame(ventana,bg="purple")
 seccion3.pack(expand=True,fill='both')
 
 #Agregamos botones
-botonAzul=Button(seccion1,text="Boton azul",bg="cyan",fg="black")
+botonAzul=Button(seccion1,text="Boton azul",bg="cyan",fg="black",command=mostrarmensaje)
 botonAzul.place(x=60,y=60,height=30,width=100)
 
 botonNegro=Button(seccion2,text="Boton Negro",bg="black",fg="white")
@@ -24,7 +36,7 @@ botonNegro.grid(row=0,column=0)
 botonAmarillo=Button(seccion2,text="Boton amarillo",bg="yellow",fg="red")
 botonAmarillo.grid(row=1,column=1)
 
-botonVerde=Button(seccion3,text="Boton verde",bg="lightgreen",fg="black")
+botonVerde=Button(seccion3,text="Boton verde",bg="#255748",fg="white",command=agregarboton)
 botonVerde.pack()
 #Metodo para la ejecucion
 ventana.mainloop()
