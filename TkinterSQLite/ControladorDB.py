f01e147f63e5099a6f51d95de7aed811fc91d9a8
PabloRC0525 <121038827@upq.edu.mx>
2023-03-24 14:06:43 -0600

    Preparamos vista de usuario

---
+++ TkinterSQLite/ControladorDB.py
@@ -0,0 +1,3 @@
+from tkinter import messagebox
+import sqlite3
+import bcrypt
---
+++ TkinterSQLite/UsuariosCRUD.py
@@ -0,0 +1,37 @@
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
+
+ventana = Tk()
+ventana.title("CRUD de usuarios")
+ventana.geometry("500x300")
+
+panel = ttk.Notebook(ventana)
+panel.pack(fill='both',expand='yes')
+
+pestaña1 = ttk.Frame(panel)
+pestaña2 = ttk.Frame(panel)
+pestaña3 = ttk.Frame(panel)
+pestaña4 = ttk.Frame(panel)
+
+#Pestaña 1: Fotmulario de registro
+titulo = Label(pestaña1,text="Registro de usuarios",fg="blue",font=("Modern",18)).pack()
+varNom = tk.StringVar()
+lblNom = Label(pestaña1,text="Nombre: ").pack()
+txtNom = Entry(pestaña1,textvariable=varNom).pack()
+varCorr = tk.StringVar()
+lblCorr = Label(pestaña1,text="Correo: ").pack()
+txtCorr = Entry(pestaña1,textvariable=varCorr).pack()
+varPass = tk.StringVar()
+lblPass = Label(pestaña1,text="Contraseña: ").pack()
+txtPass = Entry(pestaña1,textvariable=varPass).pack()
+
+btnGuard = Button(pestaña1,text="Guardar usuario").pack()
+
+
+panel.add(pestaña1,text="Formulario usuarios")
+panel.add(pestaña2,text="Buscar usuario")
+panel.add(pestaña3,text="Consultar usuarios")
+panel.add(pestaña4,text="Actualizar usuario")
+
+ventana.mainloop()
