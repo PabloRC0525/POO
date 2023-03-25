8d65112e9a5782cdb009ad0c545c57fd429d5c40
PabloRC0525 <121038827@upq.edu.mx>
2023-03-24 18:44:46 -0600

    Se Terminó

--- TkinterSQLite/ControladorDB.py
+++ TkinterSQLite/ControladorDB.py
@@ -1,3 +1,48 @@
 from tkinter import messagebox
 import sqlite3
 import bcrypt
+
+class ControladorDB:
+    
+    def __init__(self):
+        pass
+    
+    def conexionDB (self):
+        try:
+            conexion = sqlite3.connect("C:/Users/Pablo/Documents/GitHub/POO/TkinterSQLite/DBUsuarios.db")
+            print("Conectado a la DB")
+            return conexion
+        except sqlite3.OperationalError:
+            print("No se pudo conectar")
+    
+    def guardarUsuario(self,nom,corr,con):
+        #1. llamo el metodo conexion
+        conx = self.conexionDB()
+        #2. Validar vacios
+        if(nom =="" or corr == "" or con == ""):
+            messagebox.showwarning("Aguas!!","Formulario incompleto")
+            conx.close()
+        else:
+            #3. realizar el insert a la DB
+            #4. Preparamos las variables necesarias
+            cursor = conx.cursor()
+            conH = self.ecriptarContra(con)
+            datos = (nom,corr,conH)
+            sqlInsert = "insert into tbRegistrados(nombre,correo,contra) values (?,?,?)"
+            
+            #5. Ejecutamos el Insert y cerramos la conexion
+            cursor.execute(sqlInsert,datos)
+            conx.commit()
+            conx.close
+            messagebox.showinfo("Exito","Usuario guardado")
+        
+    def ecriptarContra(self,con):
+        #preparamos contraseña y sal para hash
+        conPlana = con
+        conPlana = conPlana.encode() #Contraseña convertida a bytes
+        sal = bcrypt.gensalt()
+        #encriptamos
+        conHa = bcrypt.hashpw(conPlana,sal)
+        print(conHa)
+        #regresamos la contraseña encriptada
+        return conHa
--- TkinterSQLite/UsuariosCRUD.py
+++ TkinterSQLite/UsuariosCRUD.py
@@ -1,6 +1,15 @@
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
+from ControladorDB import *
+
+#Instancia: Puente entre los 2 archivos
+controlador = ControladorDB()
+
+#Metodo que usa mi obj controlador para insertar
+
+def ejecutaInsert():
+    controlador.guardarUsuario(varNom.get(),varCorr.get(),varPass.get())
 
 ventana = Tk()
 ventana.title("CRUD de usuarios")
@@ -26,8 +35,7 @@ varPass = tk.StringVar()
 lblPass = Label(pestaña1,text="Contraseña: ").pack()
 txtPass = Entry(pestaña1,textvariable=varPass).pack()
 
-btnGuard = Button(pestaña1,text="Guardar usuario").pack()
-
+btnGuard = Button(pestaña1,text="Guardar usuario",command = ejecutaInsert).pack()
 
 panel.add(pestaña1,text="Formulario usuarios")
 panel.add(pestaña2,text="Buscar usuario")
